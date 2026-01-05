ec01d062b3475d6ecb26e8f9c4e23b7d02f459f1
Aniruddha Landge <100419083+AniruddhLandge@users.noreply.github.com>
2026-01-05 16:26:03 +0530

    Update database configuration options
    Disable logging and set the database port.

--- server/config/db.js
+++ server/config/db.js
@@ -12,7 +12,9 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false,
+    port: process.env.DB_PORT,
   }
 );
 
