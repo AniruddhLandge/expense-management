ae97757b1d9fabe86caf116339f022717e5c618d
AniruddhLandge <landgeaniruddha@gmail.com>
2026-01-08 11:44:13 +0530

    remove hasmany relationship in Group

--- server/models/Group.js
+++ server/models/Group.js
@@ -1,56 +1,23 @@
-// // server/models/Group.js
-// import { DataTypes } from "sequelize";
-// import sequelize from "../config/db.js";
-
-// const Group = sequelize.define("Group", {
-//   name: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   members: {
-//     type: DataTypes.JSON, 
-//     allowNull: false,
-//   },
-// },
-//   {
-//     tableName: "user_groups",     
-//     freezeTableName: true,  
-//     timestamps: false,
-
-//   }
-// );
-
-// export default Group;
-
-
 // server/models/Group.js
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
-import GroupExpense from "./GroupExpense.js";
 
-const Group = sequelize.define(
-  "Group",
-  {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    members: {
-      type: DataTypes.JSON,
-      allowNull: false,
-    },
+const Group = sequelize.define("Group", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
+  members: {
+    type: DataTypes.JSON, 
+    allowNull: false,
+  },
+},
   {
-    tableName: "user_groups",
-    freezeTableName: true,
-    timestamps: true, 
+    tableName: "user_groups",     
+    freezeTableName: true,  
+    timestamps: false,
+
   }
 );
-
-// ✅ REQUIRED ASSOCIATION (THIS WAS MISSING)
-Group.hasMany(GroupExpense, {
-  foreignKey: "groupId",
-  onDelete: "CASCADE",
-});
 
 export default Group;
